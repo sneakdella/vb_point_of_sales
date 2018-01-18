a0f6761d08748dc490a48520f8c9980ff05fb8d3
Yuhas <sneak.della@gmail.com>
2018-01-18 14:15:26 -0500

    init
    initial commit

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,356 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(563, 335)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(179, 62)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Hello World"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(114, 156)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 159)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Burger"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 185)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Pizza (Cheese)"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(114, 182)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Pizza (Pep)"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(114, 208)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(14, 237)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Pizza Slice"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(114, 234)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 5
+        Me.TextBox4.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 263)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Medium Drink"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(114, 260)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 6
+        Me.TextBox5.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(14, 289)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Large Drink"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(114, 286)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 7
+        Me.TextBox6.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(14, 133)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Burger (Cheese)"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(114, 130)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox7.TabIndex = 1
+        Me.TextBox7.Text = "0"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(563, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(179, 80)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Generate Receipt"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(17, 326)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(117, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Employee Discount"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(17, 349)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(109, 17)
+        Me.CheckBox2.TabIndex = 9
+        Me.CheckBox2.Text = "Member Discount"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(288, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(269, 383)
+        Me.RichTextBox1.TabIndex = 18
+        Me.RichTextBox1.Text = ""
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 15)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(82, 13)
+        Me.Label8.TabIndex = 20
+        Me.Label8.Text = "Customer Name"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(114, 12)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(168, 20)
+        Me.TextBox8.TabIndex = 10
+        Me.TextBox8.Text = "Name"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(111, 108)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(23, 13)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "Qty"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(14, 108)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(27, 13)
+        Me.Label10.TabIndex = 22
+        Me.Label10.Text = "Item"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(564, 272)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(178, 57)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Void Receipt"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(564, 208)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(178, 58)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "New Order"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(12, 46)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(53, 13)
+        Me.Label11.TabIndex = 25
+        Me.Label11.Text = "Employee"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(114, 46)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(168, 20)
+        Me.TextBox9.TabIndex = 0
+        Me.TextBox9.Text = "Employee"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(758, 411)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Cash Register Jake Yuhas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox9 As TextBox
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,164 @@
+﻿Public Class Form1
+    Public Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim strName As String
+        Dim strGreeting As String
+        Dim strEmployeeName As String
+
+        strEmployeeName = TextBox9.Text
+
+        strName = TextBox8.Text
+
+        'Will bring up a new window and say "Hello <str>"
+        strGreeting = $"Hello {strEmployeeName}"
+
+
+        MessageBox.Show(strGreeting) 'Display message on computer screen
+    End Sub
+
+    'Executes all functions to generate a receipt
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim strName As String
+        Dim strGreeting As String
+        Dim strGreeting2 As String
+        Dim strEmployeeName As String
+
+        strName = TextBox8.Text
+        strEmployeeName = TextBox9.Text
+
+        strGreeting = $"Receipt for Customer: {strName}"
+        strGreeting2 = $"Employee Name: {strEmployeeName}"
+
+        'Generates Employee and Customer lines in the RichTextBox based on what is entered in the textboxes
+        RichTextBox1.Text = strGreeting & vbNewLine & vbNewLine & strGreeting2 & vbNewLine & vbNewLine
+
+        ' Defining the values in the textboxes
+        Dim intBurgCheese As Integer
+        Dim intBurg As Integer
+        Dim intPizzaCheese As Integer
+        Dim intPizzaPep As Integer
+        Dim intPizzaSlice As Integer
+        Dim intMedDrink As Integer
+        Dim intLargeDrink As Integer
+
+        ' Defining the prices of each item
+        Dim valBurgCheese As Decimal = 2.25
+        Dim valBurg As Decimal = 2.0
+        Dim valPizzaCheese As Decimal = 5.0
+        Dim valPizzaPep As Decimal = 6.0
+        Dim valPizzaSlice As Decimal = 1.0
+        Dim valMedDrink As Decimal = 1.0
+        Dim valLargeDrink As Decimal = 2.0
+
+        ' Decimal Conversion for math
+        intBurgCheese = Convert.ToDecimal(TextBox7.Text)
+        intBurg = Convert.ToDecimal(TextBox1.Text)
+        intPizzaCheese = Convert.ToDecimal(TextBox2.Text)
+        intPizzaPep = Convert.ToDecimal(TextBox3.Text)
+        intPizzaSlice = Convert.ToDecimal(TextBox4.Text)
+        intMedDrink = Convert.ToDecimal(TextBox5.Text)
+        intLargeDrink = Convert.ToDecimal(TextBox6.Text)
+
+        'Calculations
+        Dim calcBurgCheese As Decimal = intBurgCheese * valBurgCheese
+        Dim calcBurg As Decimal = intBurg * valBurg
+        Dim calcPizzaCheese As Decimal = intPizzaCheese * valPizzaCheese
+        Dim calcPizzaPep As Decimal = intPizzaPep * valPizzaPep
+        Dim calcPizzaSlice As Decimal = intPizzaSlice * valPizzaSlice
+        Dim calcMedDrink As Decimal = intMedDrink * valMedDrink
+        Dim calcLargeDrink As Decimal = intLargeDrink * valLargeDrink
+
+        'Formatting After Calculations to be printed on the RichtTextBox
+        Dim cheeseBurger As String = vbNewLine & "Cheese Burger: " & vbTab & calcBurgCheese.ToString("0.00") & vbTab & intBurgCheese
+        Dim burger As String = vbNewLine & "Normal Burger: " & vbTab & calcBurg.ToString("0.00") & vbTab & intBurg
+        Dim pizzaCheese As String = vbNewLine & "Pizza Cheese: " & vbTab & calcPizzaCheese.ToString("0.00") & vbTab & intPizzaCheese
+        Dim pizzaPep As String = vbNewLine & "Pizza Peperoni: " & vbTab & calcPizzaPep.ToString("0.00") & vbTab & intPizzaPep
+        Dim pizzaSlice As String = vbNewLine & "Pizza Slice: " & vbTab & calcPizzaSlice.ToString("0.00") & vbTab & intPizzaSlice
+        Dim medDrink As String = vbNewLine & "Medium Drink: " & vbTab & calcMedDrink.ToString("0.00") & vbTab & intMedDrink
+        Dim largeDrink As String = vbNewLine & "Large Drink: " & vbTab & calcLargeDrink.ToString("0.00") & vbTab & intLargeDrink
+
+        ' First line before Items, Description and Amount
+        Dim prntStr As String = vbNewLine & "Item Description" & vbTab & "Amount" & vbTab & "Qty"
+        RichTextBox1.AppendText(prntStr)
+
+        ' Checks if values have changed for the amount of each product. 
+        ' This makes sure it does Not print any item the customer did Not order!
+        ' This also makes sure that if the customer did order something that it will print the formats from the lines above.
+        If intBurgCheese > 0 Then
+            RichTextBox1.AppendText(cheeseBurger)
+        End If
+        If intBurg > 0 Then
+            RichTextBox1.AppendText(burger)
+        End If
+        If intPizzaCheese > 0 Then
+            RichTextBox1.AppendText(pizzaCheese)
+        End If
+        If intPizzaPep > 0 Then
+            RichTextBox1.AppendText(pizzaPep)
+        End If
+        If intPizzaSlice > 0 Then
+            RichTextBox1.AppendText(pizzaSlice)
+        End If
+        If intMedDrink > 0 Then
+            RichTextBox1.AppendText(medDrink)
+        End If
+        If intLargeDrink > 0 Then
+            RichTextBox1.AppendText(largeDrink)
+        End If
+
+
+        'Beginning Calculations for Total
+        Dim cheeseBurgtotal As Decimal = intBurgCheese * valBurgCheese
+        Dim burgTotal As Decimal = intBurg * valBurg
+        Dim pizzaCheeseTotal As Decimal = intPizzaCheese * valPizzaCheese
+        Dim pizzaPepTotal As Decimal = intPizzaPep * valPizzaPep
+        Dim pizzaSliceTotal As Decimal = intPizzaSlice * valPizzaSlice
+        Dim medDrinkTotal As Decimal = intMedDrink * valMedDrink
+        Dim largeDrinkTotal As Decimal = intLargeDrink * valLargeDrink
+
+        'Total Before Discounts
+        Dim Total As String = Convert.ToString(cheeseBurgtotal + burgTotal + pizzaCheeseTotal + pizzaPepTotal + pizzaSliceTotal + medDrinkTotal + largeDrinkTotal)
+
+        'Extra Line to make code less redundant for Discounts on Total
+        Dim discountTotal As Decimal = (cheeseBurgtotal + burgTotal + pizzaCheeseTotal + pizzaPepTotal + pizzaSliceTotal + medDrinkTotal + largeDrinkTotal)
+
+        'Both Discounts
+        If CheckBox1.Checked And CheckBox2.Checked Then
+            Dim discountTotal4 As Decimal = (0.75 * discountTotal)
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Total:" & vbTab & Total)
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Discounted Total:" & vbTab & discountTotal4.ToString("0.00"))
+            'First Discount Only
+        ElseIf CheckBox1.Checked Then
+            Dim discountTotal2 As Decimal = (0.9 * discountTotal)
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Total:" & vbTab & Total)
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Discounted Total:" & vbTab & discountTotal2.ToString("0.00"))
+            'Second Discount Only
+        ElseIf CheckBox2.Checked Then
+            Dim discountTotal3 As Decimal = Convert.ToString(Math.Round(0.9 * discountTotal))
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Total:" & vbTab & Total)
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Discounted Total:" & vbTab & discountTotal3.ToString("0.00"))
+            'No Discounts
+        ElseIf CheckBox1.CheckState = 0 And CheckBox2.CheckState = 0 Then
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "Total:" & vbTab & Total)
+            RichTextBox1.AppendText(vbNewLine & vbNewLine & "No Discounts Provided")
+        End If
+
+    End Sub
+
+    'A mock void for a receipt
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        RichTextBox1.Text = "** Previous Receipt Voided ** "
+    End Sub
+
+    'Returns all values except Employee Name to default strings and clears RichTextBox1
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox1.Text = 0
+        TextBox2.Text = 0
+        TextBox3.Text = 0
+        TextBox4.Text = 0
+        TextBox5.Text = 0
+        TextBox6.Text = 0
+        TextBox7.Text = 0
+        TextBox8.Text = "Customer"
+        RichTextBox1.Text = Nothing
+    End Sub
+End Class
